04c6f85f36aea901032af388a9137574c60f3edc
TJ-DHBW <tim-johmann@web.de>
2021-03-05 11:38:20 +0100

    added PackageSortingCenter

--- src/main/java/packageSortingCenter/PackageSortingCenter.java
+++ src/main/java/packageSortingCenter/PackageSortingCenter.java
@@ -1,5 +1,45 @@
 package packageSortingCenter;
 
+import packageSortingCenter.centralControlUnit.CentralControlUnit;
+import packageSortingCenter.sortingFacility.SortingFacility;
+import packageSortingCenter.terminal.Terminal;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class PackageSortingCenter {
-    //TODO
+    private final CentralControlUnit centralControlUnit;
+    private final UnloadingZone[] unloadingZones;
+    private final ParkingZone parkingZone;
+    private final SortingFacility sortingFacility;
+    private final Terminal terminal;
+
+    public PackageSortingCenter() {
+        this.unloadingZones = new UnloadingZone[] {new UnloadingZone(),
+                new UnloadingZone(),
+                new UnloadingZone(),
+                new UnloadingZone(),
+                new UnloadingZone(),
+                new UnloadingZone(),
+                new UnloadingZone()
+        };
+        this.parkingZone = new ParkingZone();
+        this.sortingFacility = new SortingFacility();
+        this.terminal = new Terminal(sortingFacility);
+
+        this.centralControlUnit = new CentralControlUnit();
+        connectToCentralControlUnit();
+    }
+
+    private void connectToCentralControlUnit(){
+        //TODO connect all the stuff to the central control unit
+    }
+
+    public List<UnloadingZone> getUnloadingZonesAsList(){
+        return Arrays.asList(unloadingZones);
+    }
+
+    public Terminal getTerminal() {
+        return terminal;
+    }
 }
